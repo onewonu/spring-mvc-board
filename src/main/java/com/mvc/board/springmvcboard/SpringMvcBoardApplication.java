fa0bbb312e152285c4c1d29204c21fb25510d209
Wonwoo-Seo <so222234@gmail.com>
2025-09-17 09:42:35 +0900

    프로젝트 의존성 및 초기 설정
    - jpa
    - web
    - mysql

---
+++ src/main/java/com/mvc/board/springmvcboard/SpringMvcBoardApplication.java
@@ -0,0 +1,13 @@
+package com.mvc.board.springmvcboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcBoardApplication.class, args);
+	}
+
+}
