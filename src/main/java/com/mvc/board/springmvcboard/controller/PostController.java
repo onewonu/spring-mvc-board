68f94ae599f657b51bf617ff63f7aef9a0a7e81e
Wonwoo-Seo <so222234@gmail.com>
2025-09-22 10:04:51 +0900

    게시글 생성 폼과 생성 기능
    생성 후 리스트로 리다이렉션 (추후 상세 페이지로 변경)

--- src/main/java/com/mvc/board/springmvcboard/controller/PostController.java
+++ src/main/java/com/mvc/board/springmvcboard/controller/PostController.java
@@ -1,11 +1,15 @@
 package com.mvc.board.springmvcboard.controller;
 
+import com.mvc.board.springmvcboard.dto.PostCreateDto;
 import com.mvc.board.springmvcboard.dto.PostResponseDto;
 import com.mvc.board.springmvcboard.service.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -24,5 +28,24 @@ public class PostController {
         List<PostResponseDto> posts = postService.getAllPosts();
         model.addAttribute("posts", posts);
         return "posts/list";
+    }
+
+    @GetMapping("/new")
+    public String createPostForm(Model model) {
+        model.addAttribute("post", new PostCreateDto("", ""));
+        return "posts/create";
+    }
+
+    @PostMapping
+    public String createPost(@ModelAttribute PostCreateDto createDto, RedirectAttributes redirectAttributes) {
+        try {
+            postService.createPost(createDto);
+            redirectAttributes.addFlashAttribute("message", "게시글이 작성되었습니다.");
+            return "redirect:/posts";
+        } catch (IllegalArgumentException e) {
+            redirectAttributes.addFlashAttribute("error", e.getMessage());
+            redirectAttributes.addFlashAttribute("post", createDto);
+            return "redirect:/posts/new";
+        }
     }
 }
