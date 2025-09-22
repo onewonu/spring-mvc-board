e3a14b257b3ddeff4fe3901aced37ffc09e7cfb1
Wonwoo-Seo <so222234@gmail.com>
2025-09-22 13:25:37 +0900

    상세 폼 추가
    - 게시물 리스트의 제목으로 접근
    - 리스를 다시 호출하는 버튼 생성
    - 생성 후 상세 폼으로 가도록 리다이렉션 수정

--- src/main/java/com/mvc/board/springmvcboard/controller/PostController.java
+++ src/main/java/com/mvc/board/springmvcboard/controller/PostController.java
@@ -1,14 +1,12 @@
 package com.mvc.board.springmvcboard.controller;
 
 import com.mvc.board.springmvcboard.dto.PostCreateDto;
+import com.mvc.board.springmvcboard.dto.PostDetailDto;
 import com.mvc.board.springmvcboard.dto.PostResponseDto;
 import com.mvc.board.springmvcboard.service.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -39,13 +37,20 @@ public class PostController {
     @PostMapping
     public String createPost(@ModelAttribute PostCreateDto createDto, RedirectAttributes redirectAttributes) {
         try {
-            postService.createPost(createDto);
+            PostResponseDto savedPost = postService.createPost(createDto);
             redirectAttributes.addFlashAttribute("message", "게시글이 작성되었습니다.");
-            return "redirect:/posts";
+            return "redirect:/posts/" + savedPost.id();
         } catch (IllegalArgumentException e) {
             redirectAttributes.addFlashAttribute("error", e.getMessage());
             redirectAttributes.addFlashAttribute("post", createDto);
             return "redirect:/posts/new";
         }
+    }
+
+    @GetMapping("/{id}")
+    public String getPostDetail(@PathVariable Long id, Model model) {
+        PostDetailDto post = postService.getPostDetail(id);
+        model.addAttribute("post", post);
+        return "posts/detail";
     }
 }
